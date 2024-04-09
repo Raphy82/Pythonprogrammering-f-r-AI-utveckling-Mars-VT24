a3547495aef07d55c1b0d58643ac27fecd8aaf7a
Raphy82 <raphy.thomas82@gmail.com>
2024-04-09 07:38:53 +0200

    printout the number of words in the random list

--- Labbar/Labb_1.py
+++ Labbar/Labb_1.py
@@ -1,11 +1,12 @@
 import random
 
 class Hangman:
-    lista = ["Bagdad", "Mosul", "Basra", "Arbil", "Najaf", "Dohuk"]
+    lista = ["bagdad", "mosul", "basra", "arbil", "najaf", "dohuk"]
 
     #returnera ett slumpmässigt ord från listan
     def slump(self):
         return random.choice(lista)
     print(random.choice(lista)) # skriv ut lista i slumpmässig ordning
+    print(len(random.choice(lista))) # skriv ut hur många bokstäver ordet består av
 
 

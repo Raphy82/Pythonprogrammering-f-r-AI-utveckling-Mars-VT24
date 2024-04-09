a3a90cd713cdb69f278b61c1353116284042d6f7
Raphy82 <raphy.thomas82@gmail.com>
2024-04-09 07:23:14 +0200

    Create a list and test the printout

--- Labbar/Labb_1.py
+++ Labbar/Labb_1.py
@@ -1,1 +1,11 @@
-#Project 1
+import random
+
+class Hangman:
+    lista = ["Bagdad", "Mosul", "Basra", "Arbil", "Najaf", "Dohuk"]
+
+    #returnera ett slumpmässigt ord från listan
+    def slump(self):
+        return random.choice(lista)
+    print(random.choice(lista)) # skriv ut lista i slumpmässig ordning
+
+
